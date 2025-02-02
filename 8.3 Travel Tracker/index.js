b46be4d631ac3291e99f812edd4356d6aa1d6ea0
ritikasingh4374 <154665560+ritikasingh4374@users.noreply.github.com>
2025-02-02 19:21:03 +0530

    add countries to world_map db

---
+++ 8.3 Travel Tracker/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const db = new pg.Client({
+  user : "postgres",
+  host : "localhost",
+  database : "world2",
+  password : "Ritika#12345678",
+  port : 5432,
+});
+
+db.connect();
+
+
+app.get("/", async (req, res) => {
+  const result = await db.query("SELECT country_code FROM world_map");
+  const countries = [];
+  result.rows.forEach((country) =>{
+    countries.push(country.country_code);
+  })
+  res.render("index.ejs", { countries : countries , total : countries.length});
+});
+
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+  const result = await db.query("select (country_code) from countries where country = ($1)", [input]);
+
+  if (result.rows.length !== 0) {
+    const code = result.rows[0];
+    await db.query("INSERT INTO world_map (country_code) VALUES ($1)", [code.country_code]);
+  } 
+  res.redirect("/");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
